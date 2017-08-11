689d8022d81cca5d2ecec9759e2ae0b2be964076
mikairis <thong.michael@gmail.com>
2017-08-11 14:45:56 +0800

    first change

--- Workshop5/Program.cs
+++ Workshop5/Program.cs
@@ -8,8 +8,33 @@ namespace Workshop5
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+            Console.WriteLine("****************************Workshop 5 Task 3********************************");
+            Console.WriteLine("**This programme matches 2 sets of data from 2 strings and returns a boolean**");
+
+            string[] s1 = { "The brown fox", "The brown fox", "The brown fox", "T", "The bras basah complex" };
+            string[] s2 = { "O", "FOX", "bRO", "bRO", "bRO" };
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                string str1 = s1[i];
+                string str2 = s2[i];
+
+                string lowStr1 = str1.ToLower();
+                string lowStr2 = str2.ToLower();
+
+                bool boolCheck = InString(lowStr1, lowStr2);
+                Console.WriteLine(str1 + "\t\t\t" + str2 + "\t\t\t" + boolCheck);
+            }
+            Console.ReadKey();
+        }
+        static Boolean InString(string s1, string s2)
+        {
+            bool boolChecker = s1.Contains(s2);
+            return boolChecker;
         }
     }
 }

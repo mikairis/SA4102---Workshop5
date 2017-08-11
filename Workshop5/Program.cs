46810da784c18aac3ffeb178b18742f01374b250
Michael Thong <thong.michael@gmail.com>
2017-08-12 00:04:00 +0800

    Update Program.cs

--- Workshop5/Program.cs
+++ Workshop5/Program.cs
@@ -19,55 +19,6 @@ namespace Workshop5
             }
             Console.ReadKey();
 
-            Console.WriteLine("****************************************Workshop 5 Task 4***************************************");
-            Console.WriteLine("**This programme matches 2 sets of data from 2 strings and returns an integer for the position**");
-
-            string[] s1 = { "The brown fox", "The brown fox", "The brown fox", "T", "The bras basah complex" };
-            string[] s2 = { "O", "FOX", "bRO", "bRO", "bRO" };
-
-            for (int i = 0; i < s1.Length; i++)
-            {
-                string str1 = s1[i];
-                string str2 = s2[i];
-                bool boolCheck = InString(str1, str2);
-                if (boolCheck == true)
-                {
-                    int position = FindWord(str1, str2);
-                    Console.WriteLine(position);
-                }
-                else
-                {
-                    Console.WriteLine("-1");
-                }
-            }
-            Console.ReadKey();
-        }
-        static Boolean InString(string s1, string s2)
-        {
-            string str1 = s1.ToLower();
-            string str2 = s2.ToLower();
-            bool boolChecker = str1.Contains(str2);
-            return boolChecker;
-        }
-        static int FindWord(string str1, string str2)
-        {
-            int index = 0;
-            int j = 0;
-            string s1 = str1.ToLower();
-            string s2 = str2.ToLower();
-            char[] c = s1.ToCharArray();
-            char[] ch = s2.ToCharArray();
-
-            for (int i = 0; i < s1.Length; i++)
-            {
-                if (c[i] == ch[0])
-                {
-                    index = i;
-                    break;
-                }
-            }
-            return index;*/
-
             Console.WriteLine("******************************Workshop 5 Task 6******************************");
             Console.WriteLine("**This programme will find specific occurance in string and substitute them**");
             Console.WriteLine("*****************************************************************************");
@@ -100,7 +51,44 @@ namespace Workshop5
                 }
             }
             string newS = new string(charS);
-            return newS;
+            return newS;*/
+
+            Console.WriteLine("****************************************Workshop 5 Task 4***************************************");
+            Console.WriteLine("**This programme matches 2 sets of data from 2 strings and returns an integer for the position**");
+
+            string[] s1 = { "The brown fox", "The brown fox", "The brown fox", "T", "The bras basah complex" };
+            string[] s2 = { "O", "FOX", "bRO", "bRO", "bRO" };
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                string str1 = s1[i];
+                string str2 = s2[i];
+                bool boolCheck = InString(str1, str2);
+                if (boolCheck == true)
+                {
+                    int position = FindWord(str1, str2);
+                    Console.WriteLine(position);
+                }
+                else
+                {
+                    Console.WriteLine("-1");
+                }
+            }
+            Console.ReadKey();
+        }
+        static Boolean InString(string s1, string s2)
+        {
+            string str1 = s1.ToLower();
+            string str2 = s2.ToLower();
+            bool boolChecker = str1.Contains(str2);
+            return boolChecker;
+        }
+        static int FindWord(string s1, string s2)
+        {
+            string str1 = s1.ToLower();
+            string str2 = s2.ToLower();
+            int index = str1.IndexOf(str2);
+            return index;
         }
     }
 }
